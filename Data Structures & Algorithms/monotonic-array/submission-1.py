36304c1da4a4bd25beac936b151a6f27f8d6af7a
Sanketh Edara <34985962+Sanketh23@users.noreply.github.com>
2026-07-16 20:46:05 -0400

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        i = 1
+        if nums[1] >= nums[0]:
+            while i < len(nums):
+                if nums[i] >= nums[i-1]:
+                    i += 1
+                else:
+                    break
+            if i == len(nums):
+                return True
+        else:
+            while i < len(nums):
+                if nums[i] <= nums[i-1]:
+                    i += 1
+                else:
+                    break
+            if i == len(nums):
+                return True
+        
+        return False
