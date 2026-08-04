409bacb6cbb9d78704b11ab41db741ebbe9b296c
Sanketh Edara <34985962+Sanketh23@users.noreply.github.com>
2026-08-04 13:26:27 -0400

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count = Counter(students)
+        res = len(students)
+        bool_t = False
+        for s in sandwiches:
+            if count[s] > 0:
+                res -= 1
+                print(res)
+                count[s] -= 1
+                print(count[s], s)
+            else:
+                bool_t = True
+                temp = res
+        
+        if bool_t:
+            return temp
+        return res
+            
+        
