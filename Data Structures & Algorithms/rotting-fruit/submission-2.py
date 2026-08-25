17e4b22723ec072b2d363f1c0c7868247befb425
Sanketh Edara <34985962+Sanketh23@users.noreply.github.com>
2026-08-25 13:28:35 -0400

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,49 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        visited = set()
+        q = deque()
+        fresh = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+        
+        if fresh == 0:
+            return 0
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    visited.add((r,c))
+                    q.append((r,c))
+        
+        def checkAdjacent(r,c):
+            nonlocal fresh
+            if r < 0 or c < 0 or r >= rows or c >= cols or (r,c) in visited or grid[r][c] == 0:
+                return
+            grid[r][c] = 2
+            fresh -= 1
+            visited.add((r,c))
+            q.append((r,c))
+
+        minutes = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    checkAdjacent(r + dr, c + dc)
+                    
+            minutes += 1
+            if fresh == 0:
+                return minutes
+        
+        
+        return -1
+                
+        
+        
+
